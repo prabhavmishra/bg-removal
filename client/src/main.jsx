fb3ae4b33b9d8d79152b80e30e2c0d3d1502242b
Prabhav Mishra <prabhavmishra@gmail.com>
2025-12-29 22:54:12 +0530

    Configure ES module support for server

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,12 +14,7 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ClerkProvider 
-      publishableKey={PUBLISHABLE_KEY}
-      signInUrl="/"
-      signUpUrl="/"
-      afterSignInUrl="/"
-      afterSignUpUrl="/"
-    >
+      publishableKey={PUBLISHABLE_KEY} >
       <App />
     </ClerkProvider>
   </BrowserRouter>
