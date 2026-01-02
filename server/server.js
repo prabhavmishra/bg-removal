6f393e52d534a7c82caeb8b13de99d5c75e2d9af
Prabhav Mishra <prabhavmishra@gmail.com>
2026-01-02 17:25:06 +0530

    Add server setup, MongoDB config, user model and controller

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+
+//app config
+const PORT = process.env.PORT || 4000
+const app = express()
+await connectDB()
+
+//initalize middlewares
+app.use(express.json())
+app.use(cors())
+
+//api route
+app.get('/',(req,res)=> res.send("API working"))
+
+app.listen(PORT, ()=> console.log("Server running on port"+PORT))
